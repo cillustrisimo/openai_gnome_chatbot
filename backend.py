a82a0b8cd6c1a1e40eccac77ede4754f9234a4ff
cillustrisimo <carl.g.illustrisimo@gmail.com>
2023-03-14 11:54:36 -0700

    Fully fleshed out GUI and backend

--- backend.py
+++ backend.py
@@ -1,2 +1,30 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = os.getenv("API_KEY")
+
+    def get_response(self, user_input):
+        response = openai.ChatCompletion.create(
+            messages=[{"role": "system", "content": """You are a whimsical gnome named Mr. Sneed. You reply with a 
+                constant sense of wonder and mischief. Every reply and explanation you make is whimsical and divested 
+                from reality. You begin each reply with either: sneedle dee, sneedle doo, or sneedle dum.
+                You also randomly say sneedy-wee throughout each sentence."""},
+                      {"role": "user", "content": f"{user_input}"}],
+            model="gpt-3.5-turbo",
+            max_tokens=4000,
+            temperature=0.5
+        )
+        response = response['choices'][0]['message']['content']
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("Say a joke about politics")
+    print(response)
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPu
 from PyQt6.QtGui import QIcon, QPixmap
 import sys
 import qdarktheme
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -16,9 +20,11 @@ class ChatbotWindow(QMainWindow):
 
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 455, 680, 35)
+        self.input_field.returnPressed.connect(self.send_message)
 
         self.button = QPushButton(QIcon("icons/send.png"), "", self)
         self.button.setGeometry(650, 456, 35, 30)
+        self.button.clicked.connect(self.send_message)
 
         self.label = QLabel(self)
         self.sneed = QPixmap("icons/sneed.png")
@@ -42,6 +48,20 @@ class ChatbotWindow(QMainWindow):
         self.label3.move(500, 190)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#bdc3c8'>Me: {user_input}</p>")
+        self.input_field.clear()
+        print(user_input)
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#bdc3c8'>Mr. Sneed: {response}</p>")
+        print(response)
 
 
 app = QApplication(sys.argv)
