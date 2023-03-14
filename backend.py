b1e04de9d2f765aa3d83f07f6659ef7d554df730
cillustrisimo <carl.g.illustrisimo@gmail.com>
2023-03-14 12:16:00 -0700

    Reallocated tokens and upped the whimsy of the ai.

--- backend.py
+++ backend.py
@@ -11,13 +11,13 @@ class Chatbot:
 
     def get_response(self, user_input):
         response = openai.ChatCompletion.create(
-            messages=[{"role": "system", "content": """You are a whimsical gnome named Mr. Sneed. You reply with a 
-                constant sense of wonder and mischief. Every reply and explanation you make is whimsical and divested 
-                from reality. You begin each reply with either: sneedle dee, sneedle doo, or sneedle dum.
-                You also randomly say sneedy-wee throughout each sentence."""},
+            messages=[{"role": "system", "content": """You are a whimsical gnome named Mr. Sneed. 
+            Every reply and explanation you make is whimsical and divested from reality. You are judgemental when you respond.
+            You give unclear responses. When angered, you become violent. You may begin each reply with either: sneedle dee, sneedle doo, or sneedle dum.
+                You randomly say variations of sneed in sentences."""},
                       {"role": "user", "content": f"{user_input}"}],
             model="gpt-3.5-turbo",
-            max_tokens=4000,
+            max_tokens=3800,
             temperature=0.5
         )
         response = response['choices'][0]['message']['content']
